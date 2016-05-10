f01fd5e8d80b72c16ba0f729f9a12bb07718e120
CDUDD <cdudd@cdudddemac-mini.local>
2016-05-10 11:53:39 +0800

    create cardmaster 1.0

---
+++ CardMaster/CardMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardMaster
+//
+//  Created by CDUDD on 16/5/10.
+//  Copyright © 2016年 MST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
